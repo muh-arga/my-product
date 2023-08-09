69136a733c75062c3bb7b6216d1e2f05d150154e
= <=>
2023-08-09 22:26:00 +0700

    fix: sidebar link

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">MY-PRODUCT</a>
+            <a href={{ route('home') }}>MY-PRODUCT</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">MP</a>
+            <a href={{ route('home') }}>MP</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
